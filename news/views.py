934d65d77f33f6b5851e7df9373d230b741d46f9
farshadahn7 <farshadahn7@gmail.com>
2024-07-26 17:08:24 +0330

    done

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
