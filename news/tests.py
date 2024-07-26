d29383345c6b19aa77d4eaf7f95ef67ac1cbe26e
farshadahn7 <farshadahn7@gmail.com>
2024-07-26 15:56:51 +0330

    news data show correctly. now we could filter the news with category.

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from news.models import News, Category
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class NewsTest(TestCase):
+    def setUp(self):
+        self.tag1 = Category.objects.create(name='test')
+        self.tag2 = Category.objects.create(name='test2')
+        self.newsOne = News.objects.create(
+            title='t1',
+            content='This is t1',
+            source='t1.com',
+        )
+        self.newsOne.category.set([self.tag1, self.tag2])
+
+    def test_home_url(self):
+        res = self.client.get('/')
+        res2 = self.client.get(reverse('home'))
+        res3 = self.client.get('/?category=1')
+        self.assertEqual(res3.status_code, 200)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res2.status_code, 200)
+        self.assertEqual(self.newsOne.title, "t1")
+        self.assertEqual(self.newsOne.content, "This is t1")
+        self.assertEqual(self.newsOne.source, 't1.com')
--- news/views.py
+++ news/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
 from news.models import News
 from news.serializers import NewsSerializer
@@ -17,8 +18,17 @@ from news.serializers import NewsSerializer
 #     return Response(news_json_data.data)
 
 
-class NewsListAPIView(APIView):
-    def get(self, request):
-        news_data = News.objects.prefetch_related('category').all()
-        news_json_data = NewsSerializer(news_data, many=True)
-        return Response(news_json_data.data)
+class NewsListAPIView(ListAPIView):
+    serializer_class = NewsSerializer
+
+    # queryset = News.objects.prefetch_related('category').all()
+    # def get(self, request):
+    #     news_data = News.objects.prefetch_related('category').all()
+    #     news_json_data = NewsSerializer(news_data, many=True)
+    #     return Response(news_json_data.data)
+    def get_queryset(self):
+        news_data = News.objects.all()
+        category_id = self.request.query_params.get('category_id', None)
+        if category_id is not None:
+            news_data = news_data.filter(category__id=category_id)
+        return news_data
