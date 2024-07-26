60e23d39a92e5afdd5159e2bf5244a9fa0d29eb7
farshadahn7 <farshadahn7@gmail.com>
2024-07-26 13:22:41 +0330

    fuctional view changed to cbv

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from news import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.NewsListAPIView.as_view(), name='home'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from news.models import News
 from news.serializers import NewsSerializer
@@ -9,8 +10,15 @@ from news.serializers import NewsSerializer
 
 # class Home(TemplateView):
 #     template_name = 'home.html'
-@api_view()
-def home(request):
-    news_data = News.objects.prefetch_related('category').all()
-    news_json_data = NewsSerializer(news_data, many=True)
-    return Response(news_json_data.data)
+# @api_view()
+# def home(request):
+#     news_data = News.objects.prefetch_related('category').all()
+#     news_json_data = NewsSerializer(news_data, many=True)
+#     return Response(news_json_data.data)
+
+
+class NewsListAPIView(APIView):
+    def get(self, request):
+        news_data = News.objects.prefetch_related('category').all()
+        news_json_data = NewsSerializer(news_data, many=True)
+        return Response(news_json_data.data)
